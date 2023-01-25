0595352cd7618e5094829014d3a31c624fe62ad1
zaqaz113 <zaqaz113@naver.com>
2023-01-25 17:54:44 +0900

    구구단 웹팩

---
+++ lecture/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  name: 'wordrelay-setting',
+  mode: 'development', //개발 , 실서비스: production
+  devtool: 'eval',
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  
+  entry: {
+    app: ['./client.jsx'],
+  },
+
+  module: {
+    rules: [{
+      test: /\.jsx?/,
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env', '@babel/preset-react'],
+        plugins: ['@babel/plugin-proposal-class-properties'],
+      },
+    }],
+  },
+
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'app.js'
+  },
+};
