5f882f8e0f06dbadea9a9965316d4f0983e1f7ee
zaqaz113 <zaqaz113@naver.com>
2023-01-25 18:38:55 +0900

    구구단 웹팩 빌드 완료

---
+++ 1. 구구단/GuGuDan.jsx
@@ -0,0 +1,46 @@
+const React = require('react')
+const { useState, useRef } = React;
+
+const GuGuDan = () => {
+  const [first, setFirst] = useState(Math.ceil(Math.random() * 9));
+  const [second, setSecond] = useState(Math.ceil(Math.random() * 9));
+  const [value, setValue] = useState('');
+  const [result, setResult] = useState('');
+  const inputEl = useRef(null);
+
+  const onChangeInput = (e) => {
+    setValue(e.target.value);
+  };
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+    if (parseInt(value) === first * second) {
+      setResult('정답:' + value);
+      setFirst(Math.ceil(Math.random() * 9));
+      setSecond(Math.ceil(Math.random() * 9));
+      setValue('');
+      inputEl.current.focus();
+    } else {
+      setResult('땡');
+      setValue('');
+      inputEl.current.focus();
+    }
+  };
+
+  return (
+    <>
+      <div>
+        {first} 곱하기
+        {second}는?
+      </div>
+      <form onSubmit={onSubmitForm}>
+        <input ref={inputEl} onChange={onChangeInput} value={value} />
+        <button>입력!</button>
+      </form>
+      <div id="result">
+        {result}
+      </div>
+    </>
+  );
+};
+
+module.exports = GuGuDan;
